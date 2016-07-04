29636ef808d9b7c3e0ce778ab9539b0ac7dc5df2
Tom White <tom@sixdozen.com>
2016-07-05 00:54:31 +1200

    webapp with first github endpoint

--- web/app.py
+++ web/app.py
@@ -1,8 +1,14 @@
 import os
-from flask import Flask
+from flask import Flask, Response
+import requests
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+
+auth_params = {
+    "id": os.environ['GITHUB_ID'],
+    "secret": os.environ['GITHUB_SECRET']
+}
 
 @app.route('/')
 def hello():
@@ -11,6 +17,11 @@ def hello():
 @app.route('/settings')
 def get_settings():
     return "APP_SETTINGS: {}".format(os.environ['APP_SETTINGS'])
+
+@app.route('/members/<org>.json')
+def get_members(org):
+    r = requests.get('https://api.github.com/orgs/{}/members'.format(org), params=auth_params)
+    return Response(r.text, mimetype='application/json')
 
 if __name__ == '__main__':
     app.run()

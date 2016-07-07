9ee227861617ab533da73ee19a0b4b60b1ba36c4
Tom White <tom@sixdozen.com>
2016-07-08 00:40:27 +1200

    refactored members to use over-engineered routing

--- app.py
+++ app.py
@@ -28,12 +28,6 @@ def hello():
 def get_settings():
     return "APP_SETTINGS: {}".format(os.environ['APP_SETTINGS'])
 
-def time_get_raw_json(args):
-    return '{{"time": "{}"}}'.format(time.ctime())
-
-time_keys = [ "time" ]
-time_cache_key = "time"
-
 def fetch_and_cache_json(raw_get_fn, args, cache_key):
     rv = server_cache.get(cache_key)
     if rv is None:
@@ -51,14 +45,21 @@ def fetch_and_cache_json(raw_get_fn, args, cache_key):
 def fetch_filtered_json(raw_get_fn, args, cache_key, filter_keys):
     if cache_key == None:
         rv = raw_get_fn(args)
-        raw_map = json.loads(rv)
+        raw_list = json.loads(rv)
     else:
         rv = fetch_and_cache_json(raw_get_fn, args, cache_key)
-        raw_map = json.loads(rv)["payload"]
+        raw_list = json.loads(rv)["payload"]
     filtered_list = []
-    for k,v in raw_map.items():
-        filtered_list.append({ k: v for k in filter_keys })
+    for e in raw_list:
+        filtered_list.append({ k: e[k] for k in filter_keys })
     return json.dumps(filtered_list)
+
+# here is the core time api
+def time_get_raw_json(args):
+    return '[{{"time": "{}"}}]'.format(time.ctime())
+
+time_keys = [ "time" ]
+time_cache_key = "time"
 
 # route that is live (dynamic)
 @app.route("/time.raw.live.json")
@@ -94,51 +95,46 @@ def get_time_html():
 
 ### now do something similar for "org members"
 
-def fetch_members_json(org):
-    cache_key = 'members/{}'.format(org)
-    rv = server_cache.get(cache_key)
-    if rv is None:
-        r = requests.get('https://api.github.com/orgs/{}/members'.format(org), params=auth_params)
-        r_json = json.loads(r.text)
-        cache_time = get_time()
-        r_package = {
-            "payload": r_json,
-            "cachetime": cache_time
-        }
-        rv = json.dumps(r_package)
-        server_cache.set(cache_key, rv, timeout=default_timeout)
-    return rv
-
-@app.route('/members/<org>.raw.json')
-@cache(default_timeout)
-def get_members_raw_json(org):
-    json = fetch_members_json(org)
-    return Response(json, mimetype='application/json')
+# here is the core org members api
+def members_get_raw_json(org):
+    r = requests.get('https://api.github.com/orgs/{}/members'.format(org), params=auth_params)
+    return r.text
 
 members_keys = ["login", "avatar_url", "html_url"]
-def get_members_dict(org):
-    j = fetch_members_json(org)
-    members_list = json.loads(j)["payload"]
-    filtered_list = []
-    for d in members_list:
-        filtered_list.append({ k: d[k] for k in members_keys })
-    return filtered_list
+def members_cache_key(org):
+    return "members/{}".format(org)
 
-@app.route('/members/<org>.json')
+# route that is live (dynamic)
+@app.route("/members/<org>.raw.live.json")
+def get_members_raw_live(org):
+    return members_get_raw_json(org)
+
+@app.route("/members/<org>.raw.json")
+@cache(default_timeout)
+def get_members_raw(org):
+    return fetch_and_cache_json(members_get_raw_json, org, members_cache_key(org))
+
+@app.route("/members/<org>.live.json")
+def get_members_live_json(org):
+    return fetch_filtered_json(members_get_raw_json, org, None, members_keys)
+
+@app.route("/members/<org>.json")
 @cache(default_timeout)
 def get_members_json(org):
-    d = get_members_dict(org)
-    rv = json.dumps(d)
-    return Response(rv, mimetype='application/json')
+    return fetch_filtered_json(members_get_raw_json, org, members_cache_key(org), members_keys)
 
-@app.route('/members/<org>.html')
+@app.route("/members/<org>.live.html")
+def get_members_live_html(org):
+    j = fetch_filtered_json(members_get_raw_json, org, None, members_keys)
+    return render_template("members.html",
+                           org=org, keys=members_keys, members=json.loads(j))
+
+@app.route("/members/<org>.html")
 @cache(default_timeout)
 def get_members_html(org):
-    d = get_members_dict(org)
+    j = fetch_filtered_json(members_get_raw_json, org, members_cache_key(org), members_keys)
     return render_template("members.html",
-                           org=org,
-                           keys=members_keys,
-                           members=d)
+                           org=org, keys=members_keys, members=json.loads(j))
 
 if __name__ == '__main__':
     app.run()

eb232d377cdcd53984f4b0afb9fef2796463418d
Tom White <tom@sixdozen.com>
2016-07-08 22:48:39 +1200

    get_sha_from_branch: script to convert gist branch to sha

---
+++ scripts/get_sha_from_branch.py
@@ -0,0 +1,50 @@
+import os
+import requests
+import json
+import argparse
+import re
+
+def gist_branch_to_sha(gist_id, gist_branch):
+    # strangely, this only works when not authenticated?
+    r = requests.get('https://api.github.com/gists/{}/{}'.format(gist_id, gist_branch), stream=True)
+    content = r.raw.read(256, decode_content=True)
+    if content.startswith('{"url":'):
+        match = re.search('^{"url":"([^"]*)"', content)
+        if match == None:
+            return None
+        url = match.group(1)
+        parts = url.split("/")
+        return parts[-1]
+    else:
+        print("branch {} not found: {}".format(gist_branch, content))
+        return None
+
+if __name__ == "__main__":
+    # argparse
+    parser = argparse.ArgumentParser(description='Fetch members of an org')
+    parser.add_argument('-b','--branch', help='branch of gist', default="300b864c731f573f760835cb2fdcef2287cf8dd8")
+    parser.add_argument('-i','--id', help='id of gist', default="a36efe08f5fc152227a6cffc3aa13297")
+    parser.add_argument('-s','--secret', help='json credentials (or "none")', default="env")
+    args = parser.parse_args()
+
+    params = {}
+    if args.secret.lower() == "env":
+        params = {
+            "id": os.environ['GITHUB_ID'],
+            "secret": os.environ['GITHUB_SECRET']
+        }
+    elif args.secret.lower() != "none":
+        try:
+            with open(args.secret) as json_file:
+                params = json.load(json_file)
+        except IOError:
+            print("no secret.json file, requests will be unauthenticated")
+        except:
+            print("secret.json file could not be read, requests will be unauthenticated")
+
+    # strangely, this only works when not authenticated?
+    sha = gist_branch_to_sha(args.id, args.branch)
+    if sha != None:
+        print(sha)
+
+    
